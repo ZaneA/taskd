5d03d4af0507411cbcec1407e8f27642f6a4f9b7
Zane Ashby <zane.a@demonastery.org>
2015-07-08 00:42:51 +1200

    Rename DB.

--- run_taskd.sh
+++ run_taskd.sh
@@ -2,7 +2,7 @@
 shopt -s nullglob
 plugins=($(pwd)/plugins/*.so)
 
-export TASKD_DATABASE=taskerd.db
+export TASKD_DATABASE=taskd.db
 export TASKD_PLUGINS="${plugins[@]}"
 export TASKD_TICK_RATE=1000
 
